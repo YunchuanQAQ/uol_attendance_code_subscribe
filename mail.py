de139f5c3a8e7362fb9840a8c6a1e33ac064c467
shidifuhou <104135999+shidifuhou@users.noreply.github.com>
2022-10-14 14:00:42 +0100

    Add files via upload

---
+++ mail.py
@@ -0,0 +1,30 @@
+import os
+
+
+def send_email(user, pwd, recipient, subject, body):
+    import smtplib
+
+    FROM = user
+    TO = recipient if isinstance(recipient, list) else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(user, pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print('successfully sent the mail')
+    except:
+        print("failed to send mail")
+
+ 
+user = os.environ['user']
+pwd = os.environ['password']
+recipient = os.environ['recipient']
+send_email(user, pwd, recipient, 'test sub', 'test body')

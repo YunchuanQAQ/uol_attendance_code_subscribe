44af2bf6723b9e2488840804f5c20b88feae282a
shidifuhou <104135999+shidifuhou@users.noreply.github.com>
2022-10-20 21:52:12 +0100

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,10 +1,60 @@
 import os
-import urllib
 import http.cookiejar
-import json
-import datetime
-import pytz
+import urllib
 import time
+import json
+import pytz
+import datetime
+
+username = os.environ['username']
+password = os.environ['password']
+
+
+def submit_attendance_code(username, password, year, month, day, attCode, uniqueId, actId):
+    attStart = reformat_date_time_for_post(year, month, day, hour, minute)
+    attEnd = reformat_date_time_for_cookies(year, month, day, hour + 1, minute)
+
+    url = 'https://timetables.liverpool.ac.uk/services/register-attendance-student'
+
+    data = {'Username': username,
+            'Password': password,
+            'attCode': attCode,
+            'attCodeInput': attCode,
+            'uniqueId': uniqueId,
+            'actId': actId,
+            'attStart': attStart,
+            'attEnd': attEnd,
+            'location': ''
+            }
+    post_data = urllib.parse.urlencode(data).encode('utf-8')
+
+    headers = {
+        'User-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'}
+
+    login_url = 'https://timetables.liverpool.ac.uk/account?returnUrl=%2F'
+
+    req = urllib.request.Request(login_url, headers=headers, data=post_data)
+
+    cookie = http.cookiejar.CookieJar()
+
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie))
+
+    try:
+        resp = opener.open(req)
+    except Exception:
+        print('There might be some problem with your network.')
+        print('Check you network setting and rerun the program.')
+        quit()
+
+    req = urllib.request.Request(url, headers=headers, data=post_data)
+
+    resp = opener.open(req)
+
+    result = resp.read().decode('utf-8')
+    if 'Username:' in result:
+        result = 'password error'
+
+    return result
 
 
 def simulate_login(username, password, year, month, day):
@@ -62,7 +112,7 @@ def force_number_two_digits(number):
     return result
 
 
-def reformat_date_time(year, month, day, hour, minute):
+def reformat_date_time_for_cookies(year, month, day, hour, minute):
     year_str = str(year)
     month_str = force_number_two_digits(month)
     day_str = force_number_two_digits(day)
@@ -73,53 +123,28 @@ def reformat_date_time(year, month, day, hour, minute):
     return result
 
 
-def get_class_info(html, date_time):
-    html_json_load = (json.loads(html))
-    result_list = []
+def reformat_date_time_for_post(year, month, day, hour, minute):
+    year_str = str(year)
+    month_str = force_number_two_digits(month)
+    day_str = force_number_two_digits(day)
+    hour_str = force_number_two_digits(hour)
+    minute_str = force_number_two_digits(minute)
+
+    result = day_str + '/' + month_str + '/' + year_str + ' ' + hour_str + ':' + minute_str
+    return result
+
+
+def extract_info_from_html(html, date_time, attribute):
+    html_json_load = json.loads(html)
+    result = ''
     for one_class in html_json_load:
         if one_class['start'] == date_time:
-            result_list.append(one_class['start'])
-            result_list.append(one_class['activitydesc'])
-            result_list.append(one_class['attendancecode'])
+            result = one_class[attribute]
         else:
             continue
 
-    if len(result_list) == 0:
-        result_list = ['', '', '']
+    return result
 
-    return result_list
-
-
-def send_email(user, pwd, recipient, subject, body):
-    import smtplib
-
-    FROM = user
-    TO = recipient if isinstance(recipient, list) else [recipient]
-    SUBJECT = subject
-    TEXT = body
-
-    # Prepare actual message
-    message = """From: %s\nTo: %s\nSubject: %s\n\n%s
-    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
-    try:
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.ehlo()
-        server.starttls()
-        server.login(user, pwd)
-        server.sendmail(FROM, TO, message)
-        server.close()
-        print('successfully sent the mail')
-    except:
-        print("failed to send mail")
-
-
-start_perf = time.perf_counter()
-
-sender_email = os.environ['sender_email']
-sender_password = os.environ['sender_password']
-recipient = os.environ['recipient']
-username = os.environ['username']
-password = os.environ['password']
 
 time_zone = pytz.timezone('Europe/London')
 now = datetime.datetime.now(tz=time_zone)
@@ -128,47 +153,27 @@ year = now.year
 month = now.month
 day = now.day
 hour = now.hour
+minute = now.minute
 
-# wait until 8:55 to search code
-if hour == 8:
-    while now.minute < 55:
-        time.sleep(max(55 - now.minute, 0) * 60)
-
-found_attendance_code = 'nothing'
-
-# stop searching after 18:00
-while hour < 25:
-    html = simulate_login(username, password, year, month, day)
-    formatted_date_time = reformat_date_time(year, month, day, hour, 0)
-
-    class_info = get_class_info(html, formatted_date_time)
-    start_time = class_info[0]
-    class_name = class_info[1]
-    attendance_code = class_info[2]
-    email_content = 'The attendance code for ' + class_name + ' on ' + start_time + ' is ' + attendance_code
-
-    if attendance_code == found_attendance_code:
-        print('new attendance code not found')
-        # time.sleep(max(55 - now.minute, 0) * 60)
-        time.sleep(60)
-    elif attendance_code != '':
-        send_email(sender_email, sender_password, recipient, 'Attendance code found', email_content)
-        found_attendance_code = attendance_code
+while hour < 18:
+    if minute < 50:
+        time.sleep(300)
     else:
-        print('attendance code not found')
-        time.sleep(30)
+        # Get information
+        html = simulate_login(username, password, year, month, day)
+        formatted_date_time = reformat_date_time_for_cookies(year, month, day, hour, minute)
+        attendance_code = extract_info_from_html(html, formatted_date_time, 'attendancecode')
+        uniqueId = extract_info_from_html(html, formatted_date_time, 'uniqueid')
+        actId = extract_info_from_html(html, formatted_date_time, 'activityid')
 
-    time.sleep(30)
+        submit_attendance_code(username, password, year, month, day, attendance_code, uniqueId, actId)
+
+        time.sleep(1800)
+        
     now = datetime.datetime.now(tz=time_zone)
     print(now)
     year = now.year
     month = now.month
     day = now.day
     hour = now.hour
-
-    now_perf = time.perf_counter()
-    running_time = now_perf - start_perf
-    print('running time = ' + str(running_time))
-    if running_time > 18000:
-        print('time limit reached')
-        break
+    minute = now.minute

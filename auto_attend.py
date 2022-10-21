36f403a76be6f9dbde18f853155f1dd8e9e04382
shidifuhou <104135999+shidifuhou@users.noreply.github.com>
2022-10-21 01:19:00 +0100

    Update auto_attend.py

--- auto_attend.py
+++ auto_attend.py
@@ -165,10 +165,10 @@ while hour < 18:
         attendance_code = extract_info_from_html(html, formatted_date_time, 'attendancecode')
         uniqueId = extract_info_from_html(html, formatted_date_time, 'uniqueid')
         actId = extract_info_from_html(html, formatted_date_time, 'activityid')
-        activitydesc = extract_info_from_html(html,formatted_date_time,'activitydesc')
-        start = extract_info_from_html(html,formatted_date_time,'start')
+        activitydesc = extract_info_from_html(html, formatted_date_time, 'activitydesc')
+        start = extract_info_from_html(html, formatted_date_time, 'start')
 
-        submit_attendance_code(username, password, year, month, day, attendance_code, uniqueId, actId)
+        print(submit_attendance_code(username, password, year, month, day, attendance_code, uniqueId, actId))
         print('The attendance code of ' + activitydesc + ' on ' + start + 'has been successfully submitted.')
 
         time.sleep(1800)
